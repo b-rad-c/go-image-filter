897d1608d9adb92414d5c3c1a7cdddbdc12a156d
Brad R Corlett <bradcorlett@gmail.com>
2021-03-21 15:00:46 -0700

    add sort mode, remove pixel struct

--- imageFilter.go
+++ imageFilter.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	_ "image/jpeg"
 	"image/png"
 	"log"
 	"os"
+	"path/filepath"
+	"sort"
 
 	"github.com/urfave/cli/v2"
 )
@@ -33,7 +36,7 @@ func main() {
 				Name:        "output",
 				Aliases:     []string{"out"},
 				Usage:       "Output path for generated file (png)",
-				Required:    true,
+				Required:    false,
 				Destination: &output,
 			},
 		},
@@ -76,16 +79,7 @@ func main() {
 // common
 //
 
-type Pixel struct {
-	R int
-	G int
-	B int
-	A int
-	X int
-	Y int
-}
-
-func findMinValue(values []int) int {
+func findMinValue(values []uint32) uint32 {
 	min := values[0]
 	for _, value := range values {
 		if value < min {
@@ -95,7 +89,7 @@ func findMinValue(values []int) int {
 	return min
 }
 
-func findMaxValue(values []int) int {
+func findMaxValue(values []uint32) uint32 {
 	max := values[0]
 	for _, value := range values {
 		if value > max {
@@ -105,18 +99,7 @@ func findMaxValue(values []int) int {
 	return max
 }
 
-func rgbaToPixel(r uint32, g uint32, b uint32, a uint32, x int, y int) Pixel {
-	return Pixel{
-		R: int(r / 257),
-		G: int(g / 257),
-		B: int(b / 257),
-		A: int(a / 257),
-		X: x,
-		Y: y,
-	}
-}
-
-func load(srcPath string) ([][]Pixel, int, int) {
+func load(srcPath string) (image.Image, int, int) {
 	file, _ := os.Open(srcPath)
 	defer file.Close()
 	img, _, err := image.Decode(file)
@@ -126,64 +109,61 @@ func load(srcPath string) ([][]Pixel, int, int) {
 	}
 
 	bounds := img.Bounds()
-	width, height := bounds.Max.X, bounds.Max.Y
+	height, width := bounds.Max.Y, bounds.Max.X
 
-	var pixels [][]Pixel
-	for y := 0; y < height; y++ {
-		var row []Pixel
-		for x := 0; x < width; x++ {
-			r, g, b, a := img.At(x, y).RGBA()
-			px := rgbaToPixel(r, g, b, a, x, y)
-			row = append(row, px)
-		}
-		pixels = append(pixels, row)
+	return img, height, width
+}
+
+func save(outPath string, outImg image.Image) {
+	f, err := os.Create(outPath)
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	return pixels, height, width
+	png.Encode(f, outImg)
 }
 
 //
-// filters
-//
 
 func rowFilter(srcPath string, outPath string, mode string, shadowMask int) {
-	pixels, height, width := load(srcPath)
+	img, height, width := load(srcPath)
 
 	upLeft := image.Point{0, 0}
 	lowRight := image.Point{width, height}
 
 	outImg := image.NewRGBA(image.Rectangle{upLeft, lowRight})
 
-	for _, row := range pixels {
-		redValues := []int{}
-		greenValues := []int{}
-		blueValues := []int{}
+	for y := 0; y < height; y++ {
+		redValues := []uint32{}
+		greenValues := []uint32{}
+		blueValues := []uint32{}
 
-		avgRed := 0
-		avgGreen := 0
-		avgBlue := 0
+		avgRed := uint32(0)
+		avgGreen := uint32(0)
+		avgBlue := uint32(0)
 
-		for _, w := range row {
-			redValues = append(redValues, w.R)
-			greenValues = append(greenValues, w.G)
-			blueValues = append(blueValues, w.B)
+		for x := 0; x < width; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+
+			redValues = append(redValues, r)
+			greenValues = append(greenValues, g)
+			blueValues = append(blueValues, b)
 
 			if mode == "avg" {
-				avgRed += w.R
-				avgGreen += w.G
-				avgBlue += w.B
+				avgRed += r
+				avgGreen += g
+				avgBlue += b
 			}
 
 		}
 
-		var rValue int
-		var gValue int
-		var bValue int
+		var rValue uint32
+		var gValue uint32
+		var bValue uint32
 
 		if mode == "avg" {
-			rValue = avgRed / len(row)
-			gValue = avgGreen / len(row)
-			bValue = avgBlue / len(row)
+			rValue = avgRed / uint32(width)
+			gValue = avgGreen / uint32(width)
+			bValue = avgBlue / uint32(width)
 		} else if mode == "min" {
 			rValue = findMinValue(redValues)
 			gValue = findMinValue(greenValues)
@@ -192,24 +172,34 @@ func rowFilter(srcPath string, outPath string, mode string, shadowMask int) {
 			rValue = findMaxValue(redValues)
 			gValue = findMaxValue(greenValues)
 			bValue = findMaxValue(blueValues)
+		} else if mode == "sort" {
+			sort.Slice(redValues, func(i, j int) bool { return redValues[i] < redValues[j] })
+			sort.Slice(greenValues, func(i, j int) bool { return greenValues[i] < greenValues[j] })
+			sort.Slice(blueValues, func(i, j int) bool { return blueValues[i] < blueValues[j] })
 		} else {
 			log.Fatal("Unknown mode")
 		}
 
-		for _, w := range row {
-			y, _, _ := color.RGBToYCbCr(uint8(w.R), uint8(w.G), uint8(w.B))
-			if y < uint8(shadowMask) {
-				outImg.SetRGBA(w.X, w.Y, color.RGBA{uint8(w.R), uint8(w.G), uint8(w.B), 255})
+		for x := 0; x < width; x++ {
+			r, g, b, _ := img.At(x, y).RGBA()
+			lum, _, _ := color.RGBToYCbCr(uint8(r/257), uint8(g/257), uint8(b/257))
+
+			if lum < uint8(shadowMask) {
+				outImg.SetRGBA(x, y, color.RGBA{uint8(r / 257), uint8(g / 257), uint8(b / 257), 255})
+			} else if mode == "sort" {
+				outImg.SetRGBA(x, y, color.RGBA{uint8(redValues[x] / 257), uint8(greenValues[x] / 257), uint8(blueValues[x] / 257), 255})
 			} else {
-				outImg.SetRGBA(w.X, w.Y, color.RGBA{uint8(rValue), uint8(gValue), uint8(bValue), 255})
+				outImg.SetRGBA(x, y, color.RGBA{uint8(rValue / 257), uint8(gValue / 257), uint8(bValue / 257), 255})
 			}
 
 		}
 	}
 
-	f, err := os.Create(outPath)
-	if err != nil {
-		log.Fatal(err)
+	if outPath == "" {
+		var extension = filepath.Ext(srcPath)
+		var name = srcPath[0 : len(srcPath)-len(extension)]
+		outPath = fmt.Sprint(name, "-", mode, "-shadow-", shadowMask, ".png")
 	}
-	png.Encode(f, outImg)
+
+	save(outPath, outImg)
 }
